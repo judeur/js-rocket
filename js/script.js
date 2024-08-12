c9ab3e21025f55a515ccb0abe6c2f7b45ecb71dc
Judith Deurvorst <judeur@gmail.com>
2024-08-12 11:19:26 +0200

    Add css and some interactivity

---
+++ js/script.js
@@ -0,0 +1,24 @@
+console.log("Hallo allemaal");
+
+var car = {
+  make: "VW",
+  type: "Polo",
+  color: "Red",
+  isTurnedOn: false,
+  numberOfWheels: 4,
+  seats: ["seat 1", "seat 2", "seat 3", "seat 4"],
+  turnOn: function () {
+    this.isTurnedOn = true;
+  },
+  fly: function () {
+    alert("fly");
+  },
+  switchCar: function (isOn) {
+    console.log("turn car " + isOn);
+    if (isOn == true) {
+      this.isTurnedOn = true;
+    } else {
+      this.isTurnedOn = false;
+    }
+  },
+};
